eb35f991fbe20ca4289cc48c06435816436cf8ca
Alabala-commits <66037047+Alabala-commits@users.noreply.github.com>
2020-05-30 14:42:07 +0300

    added features of the calculator

--- InteractRunner.java
+++ InteractRunner.java
@@ -1,26 +1,120 @@
 import java.util.Scanner;
 
-public class InteractRunner {	// Класс для запуска калькулятора. Поддерживает ввод пользователя
+public class InteractRunner {
+	/*
+	* Класс для запуска калькулятора NewCalculator.java
+	* Поддерживает ввод пользователем целочисленных значений int,
+	* выбор арифметической операции "+" "-" "*" "/",
+	* возможность использования полученного результата
+	*/
+	static Scanner reader = new Scanner(System.in);
+	static NewCalculator calc = new NewCalculator();
+	static String exit = "n";
 	
-	public static void main(String[] arg) {
-		Scanner reader = new Scanner(System.in);
+	public static void main(String[] args) {		
 		try {
-			NewCalculator calc = new NewCalculator();
-			String exit = "no";
-			while(!exit.equals("yes")) {
-				System.out.println("Enter first argument : ");
-				String first = reader.next();
-				System.out.println("Enter second argument : ");
-				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
-				System.out.println("Result of addition \"+\" : " + calc.getResult());
-				calc.cleanResult();
-				System.out.println("EXIT ?");
-				System.out.println("Enter :  yes / no ");
-				exit = reader.next();
+			while(!exit.equals("y")) {
+				int firstNumber;
+				if(calc.getResult() == 0) {
+					System.out.print("Enter 1-st parametr -> ");
+					firstNumber = getParams();
+				} else 
+					firstNumber = processingResult();
+ 				System.out.print("Enter 2-st parametr -> ");
+				int secondNumber = getParams();
+				selectOper(firstNumber, secondNumber);
+				System.out.println("Result of " + calc.getName() + " = " + calc.getResult());				
+				exit = getExit();
 			}
 		} finally {
 			reader.close();
 		}
 	}
+
+	/*
+	* 		Получение параметров
+	*/
+	public static int getParams() {
+		int param = 0;	
+		
+		if(reader.hasNextInt())
+			param = reader.nextInt();
+		else {
+			System.out.print("Error simbol ! Try again -> ");
+			reader.next();
+			param = getParams();		//	Рекурсия
+		}
+		return param;
+	}
+	
+	/*
+	* 		Выбор арифметичкой операции
+	*/
+	public static void selectOper(int fist, int second) {
+		System.out.println("Select an arithmetic operation");
+		System.out.print("enter \" + \" , \" - \" , \" * \" , \" /\" -> ");
+		char operation;
+		if(reader.hasNext()) {												//  Эта штука не работает
+			operation = reader.next().charAt(0);							//	Не обрабатывает неверный ввод
+			if(operation == '+') calc.add(fist, second);
+			if(operation == '-') calc.sub(fist, second);
+			if(operation == '*') calc.mult(fist, second);
+			if(operation == '/') calc.div(fist, second);
+		} else {
+			System.out.print("Error simbol ! Try again -> ");
+			reader.next();
+			selectOper(fist, second);
+		}
+	}
+	
+	/*
+	* 		Обработка результата
+	*/
+	public static int processingResult() {
+		System.out.println("");
+		System.out.println("Result of " + calc.getName() + " = " + calc.getResult());
+		System.out.println("Use this paranetr ?");
+		String processRes = changeUser();
+		int proRes;
+		
+		if(processRes.equals("y")) {
+			proRes = calc.getResult();
+			System.out.println("1-st parametr = " + proRes);
+		} else {
+			calc.cleanResult();
+			System.out.print("Enter 1-st parametr -> ");
+			proRes = getParams();
+		}
+		return proRes;
+	}
+	
+	/*
+	* 		Обработка выбора пользователя
+	*/
+	public static String changeUser() {
+		System.out.print("enter yes or no : y / n -> ");
+		String answer = "n";
+		char answ = 'n';
+		
+		if(reader.hasNext()) 									//  Эта штука не работает
+			answ = reader.next().charAt(0);						//	Не обрабатывает неверный ввод
+		else {
+			System.out.print("Error simbol ! Try again -> ");
+			reader.next();
+			answer = changeUser();								//	Рекурсия
+		}
+		answer = Character.toString(answ);
+		return answer;
+	}
+	
+	/*
+	* 		Обработка выхода
+	*/
+	public static String getExit() {
+		System.out.println("");
+		System.out.println("EXIT ?");
+		String answer = changeUser();
+		return answer;
+	}
+	
 }
--- NewCalculator.java
+++ NewCalculator.java
@@ -1,18 +1,51 @@
 public class NewCalculator {
-
-	private int result;
-	
-	public void add(int ... params) {		// Суммируем каждый переданный аргумент
-		for(Integer param : params) {
-			this.result += param;
+	/*
+	*Класс калькулятор, реализация из командной строки
+	*/
+		private int result = 0;
+		private String nameOperation;
+		
+	/*
+	*	Cложение (addition)
+	*/
+		public void add(int param, int params) {
+			this.nameOperation = "addition \"+\"";
+			this.result = param + params;
 		}
-	}
 
-	public int getResult() {
-		return this.result;
-	} 
+	/*
+	*	Вычитание (subtraction)
+	*/
+		public void sub(int param, int params) {
+			this.nameOperation = "subtraction \"-\"";
+			this.result = param - params;
+			}
 
-	public void cleanResult() {
-		this.result = 0;
-	}
+	/*
+	*	Умножение (multiplication)
+	*/
+		public void mult(int param, int params) {
+			this.nameOperation = "multiplication \" * \"";
+			this.result = param * params;
+		}
+
+	/*
+	*	Деление(division)
+	*/
+		public void div(int param, int params) {
+			this.nameOperation = "division \" / \"";
+			this.result = param / params;
+		}
+
+		public String getName() {
+			return this.nameOperation;
+		}
+
+		public int getResult() {
+			return this.result;
+		} 
+
+		public void cleanResult() {
+			this.result = 0;
+		}
 }
